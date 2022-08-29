aeb2288469773fdd456d5d15b2e5bec64d0d1fc8
kimwoojin <kimwoojin08@naver.com>
2022-08-29 15:49:20 +0900

    20220829

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function () {
+    ////////////////////
+    $('.top_close_btn').on('click', function () {
+        $('.TopBanner').addClass('on')
+    });
+
+    $('.main_slider').slick({
+        arrows: false,
+    });
+    $(window).on('scroll', function () {
+        var sct = $(window).scrollTop();
+        sct > 0 ? $('.Header').addClass('on')
+            : $('.Header').removeClass('on')
+    })
+
+
+
+    ///////////////////
+})
